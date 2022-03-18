53eaf12a1883ff047a91bc45a2f154d11048398e
ghiath2 <ghiathht3@gmail.com>
2022-03-18 19:24:48 +0000

    Add code for correct answer

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,9 +20,11 @@ continue_btn.onclick = ()=>{
     info_box.classList.remove("activeInfo"); //hide info box
     quiz_box.classList.add("activeQuiz"); //show quiz box
     showQuestions(0);
+    queCounter(1);   
 }
 
 let que_count = 0;
+let que_numb = 1;
 
 const next_btn = quiz_box.querySelector(".next_btn");
 
@@ -30,7 +32,9 @@ const next_btn = quiz_box.querySelector(".next_btn");
 next_btn.onclick = ()=>{
   if( que_count < questions.length - 1){
     que_count++;
+    que_numb++;
     showQuestions(que_count);
+    queCounter(que_numb);
   }else{
       console.log("Questions completed")
   }
@@ -41,12 +45,67 @@ next_btn.onclick = ()=>{
 function showQuestions (index){
     const que_text = document.querySelector(".que_text");
     const option_list = document.querySelector(".option_list");
-    let que_tag = '<span>'+ questions[index].question +'</span>';
+    let que_tag = '<span>'+ questions[index].numb + ". " + questions[index].question +'</span>';
     let option_tag = '<div class="option">'+ questions[index].options[0] +'<span></span></div>'
                     +'<div class="option">'+ questions[index].options[1] +'<span></span></div>'
                     +'<div class="option">'+ questions[index].options[2] +'<span></span></div>'
                     +'<div class="option">'+ questions[index].options[3] +'<span></span></div>';
     que_text.innerHTML = que_tag;
     option_list.innerHTML = option_tag;
+    const option = option_list.querySelectorAll(".option")
+    for (let i = 0; i < option.length; i++) {
+        option[i].setAttribute("onclick", "optionSelected(this)");   
+    }
 }
+
+function optionSelected(answer){
+    let userAns = answer.textContent;
+    let correctAns = questions[que_count].answer;
+    console.log(correctAns);
+}
+
+
+
+
+
+
+
+
+
+
+function queCounter(index){
+    const bottom_ques_counter = quiz_box.querySelector(".total_que");
+    let totalQuesCountTag = '<span><p>'+ index +'</p>Of <p>'+ questions.length +'</p>Questions</span>';
+    bottom_ques_counter.innerHTML = totalQuesCountTag;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
