657a50599f8df30300b9fc8ad27f4d751ff8680b
ghiath2 <ghiathht3@gmail.com>
2022-03-18 16:56:18 +0000

    Add code to exit or qontinue the game

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,23 @@
 // Getting All Requierd Elements
-const start_btn = document.querySelector(".stary-btn button");
-const info_box = document.querySelector(".info-box");
-const exit_btn = info-box.querySelector(".buttons .quit");
-const continue_btn = document.querySelector(".stary-btn button");
+const start_btn = document.querySelector(".start_btn button");
+const info_box = document.querySelector(".info_box");
+const exit_btn = info_box.querySelector(".buttons .quit");
+const continue_btn = info_box.querySelector(".buttons .restart");
+const quiz_box = document.querySelector(".quiz_box");
 
-// If Start Quiz Clicked
+// if Start Quiz button clicked
 start_btn.onclick = ()=>{
-    info-box.classList.add("activeInfo");
+    info_box.classList.add("activeInfo"); //show info box
+};
+
+// if exitQuiz button clicked
+exit_btn.onclick = ()=>{
+    info_box.classList.remove("activeInfo"); //hide info box
 }
+
+// if Continue button clicked
+continue_btn.onclick = ()=>{
+    info_box.classList.remove("activeInfo"); //hide info box
+    quiz_box.classList.add("activeQuiz"); //show quiz box
+}
+
