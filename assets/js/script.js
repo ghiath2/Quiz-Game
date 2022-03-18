cae1a3bd5b6c2ca4004767d148d09fd51e04aa3c
ghiath2 <ghiathht3@gmail.com>
2022-03-18 15:59:51 +0000

    Add code to start the game

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+// Getting All Requierd Elements
+const start_btn = document.querySelector(".stary-btn button");
+const info_box = document.querySelector(".info-box");
+const exit_btn = info-box.querySelector(".buttons .quit");
+const continue_btn = document.querySelector(".stary-btn button");
+
+// If Start Quiz Clicked
+start_btn.onclick = ()=>{
+    info-box.classList.add("activeInfo");
+}
